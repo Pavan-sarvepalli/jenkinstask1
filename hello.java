ff02e068d50291377d354552acee635116f4c6f5
pavan sarvepalli <sarvepallipavan55@gmail.com>
2025-04-17 15:51:51 +0000

    changed class name

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
-public class HelloJava {
+public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("hello java");
     }
 }
